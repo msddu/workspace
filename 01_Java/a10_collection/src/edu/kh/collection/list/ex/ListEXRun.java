5e0f3fd8697bec50546ab16c6bb6b7b762c0ad9a
ms_ddu <alstn25000@naver.com>
2023-08-08 16:09:37 +0900

    0808컬렉션

---
+++ 01_Java/a10_collection/src/edu/kh/collection/list/ex/ListEXRun.java
@@ -0,0 +1,17 @@
+package edu.kh.collection.list.ex;
+
+public class ListEXRun {
+	
+	public static void main(String[] args) {
+		
+		ListEX ls = new ListEX();
+		
+		ls.ex1();
+		
+		
+		
+		
+		
+	}
+
+}
