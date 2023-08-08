1c25d1c514bba70a3edf30e54ea5455eeb145eea
ms_ddu <alstn25000@naver.com>
2023-08-08 16:09:17 +0900

    0808예외

---
+++ 01_Java/a09_exception/src/edu/kh/exceprion/run/ExceptionRun.java
@@ -0,0 +1,21 @@
+package edu.kh.exceprion.run;
+
+import edu.kh.exceprion.model.service.ExceptionService;
+
+public class ExceptionRun {
+	
+	public static void main(String[] args) {
+		
+		ExceptionService EX = new ExceptionService();
+		
+		EX.ex8();
+		
+		
+		
+		
+		
+	}
+	
+
+}
+
