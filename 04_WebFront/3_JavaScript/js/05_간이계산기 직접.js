82309b475c87ba85d95240358734a3f20f286187
김민수 <>
2023-09-18 22:03:10 +0900

    0918(간이계산기 만들어보기)

---
+++ 04_WebFront/3_JavaScript/js/05_간이계산기 직접.js
@@ -0,0 +1,10 @@
+function plus(){
+    const input1 = document.getElementById("input1");
+    const input2= document.getElementById("input2");
+
+    document.getElementById("result").innerText = Number(input1.value) + Number(input2.value);
+
+
+
+    
+}
