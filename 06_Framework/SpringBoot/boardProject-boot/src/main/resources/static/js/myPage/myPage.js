e06575e432a882938c2c8f8368bfb4fdb6cf04da
ms_ddu <alstn25000@naver.com>
2023-11-02 10:02:45 +0900

    git 캐시 초기화

--- 06_Framework/SpringBoot/boardProject-boot/src/main/resources/static/js/myPage/myPage.js
+++ 06_Framework/SpringBoot/boardProject-boot/src/main/resources/static/js/myPage/myPage.js
@@ -22,3 +22,55 @@ function sample6_execDaumPostcode() {
         }
     }).open();
 }
+
+//--------------------------------------------------------------------------------
+
+/* 프로필 이미지 미리보기, 제거 */
+const profileImg = document.querySelector("#profileImg"); // img 태그
+const imageInput = document.querySelector("#imageInput"); // input 태그
+const deleteImage = document.querySelector("#deleteImage"); // x 태그
+
+if(imageInput != null) { // #imageInput 존재할 때 
+
+    /* 프로필 이미지 변경(선택) 시 수행할 함수 */
+    const changeImageFn = e => {
+
+        console.log(e.target); // input태그
+        console.log(e.target.value); // value -> 업로드 파일 경로가 나옴(fakepath 형태로 출력) // C:\fakepath\sample5.jpg
+
+        /* 이게 중요! 파일의 정보가 담긴 배열이 담겨있기 때문에!*/
+        console.log(e.target.files); // 업로드된 파일의 정보가 담긴 배열 반환
+                                     // *실제 파일 *
+
+        console.log(e.target.files[0]); // 업로드된 파일 중 첫 번째 파일
+
+        const uploadFile = e.target.files[0]; 
+
+        // --------------------------- 선택된 파일의 크기가 지정된 크기를 초과하는 경우 -----------------------------------------
+        const maxSize = 1024 * 1024 ; // 1MB (byte 단위)
+
+        if(uploadFile.size > maxSize) { 
+            alert("1MB 이하의 이미지만 업로드 가능합니다");
+
+            // 최대 크기를 초과해도 input에 value가 남기 때문에 
+            // 이를 제거하는 코드가 필요하다! 
+            imageInput.value = ''; // value 삭제
+                                // 동시에 files도 삭제됨
+
+            return;
+        }
+
+        // ----------- 선택된 이미지 파일을 읽어와 미리 보기 만들기 --------------
+        
+
+
+
+    }
+
+
+
+    /* 이미지 선택 버튼을 클릭하여 선택된 파일이 변했을 때 함수 수행  */
+
+    // change 이벤트 : input의 이전 값과 현재 값이 다를 때 발생
+    imageInput.addEventListener("change", changeImageFn);
+}
